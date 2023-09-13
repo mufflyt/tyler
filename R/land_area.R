ad36e048944d3287bbb5202e125d67a6dfe8a993
Mufflyt <tyler.muffly@dhha.org>
2023-09-12 19:48:23 -0600

    Updates from 9/11/2023.

--- R/land_area.R
+++ R/land_area.R
@@ -1,11 +1,10 @@
 #' Square miles and kilometers by state.
 #'
-#' Attractiveness judgements of same-sex and other-sex self-resembling faces from DeBruine (2004).
-#'
-#' @format A data frame with 108 rows and 16 variables:
-#' \describe{
-#'   \item{col1}{col1 description}
-#'   \item{col2}{col2 description}
-#' }
-#' @source \url{https://osf.io/3c5s4/}
-"land_area"
+#' @name land_area_data
+#' @export
+
+library(tidyverse)
+library(janitor)
+library(dplyr)
+library(magrittr)
+land_area <- source("data-raw/land_area.R")
