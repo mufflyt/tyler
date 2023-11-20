05fd7ec78cbb4190883ea90d624f3c1675382960
Mufflyt <tyler.muffly@dhha.org>
2023-11-19 19:17:48 -0700

    Jesus.

--- R/this_one_works.R
+++ R/this_one_works.R
@@ -16,89 +16,84 @@
 #' @import jsonlite
 #' @import dplyr
 #' @import memoise
-scrape_physicians_data <- function(startID, endID) {
+# Function to scrape physicians' data using Tor
+scrape_physicians_data_with_tor <- function(startID, endID, torPort) {
   require(httr)
   require(jsonlite)
   require(dplyr)
   require(memoise)
 
-  # Load the data for wrong IDs
-  wrongs1 <- readr::read_csv("/Users/tylermuffly/Dropbox (Personal)/workforce/MASTER_all_wrongs.csv") %>%
-    select(WrongIDs) %>% arrange(WrongIDs) %>% filter(WrongIDs < 9044000)
-
-  # Create a list of wrong IDs to exclude from scraping
-  excluded_ids <- as.integer(wrongs1$WrongIDs)
-
-  # Generate the list of IDs to scrape, excluding wrong IDs
+  # Create a sequence of IDs from startID to endID
   id_list <- seq(startID, endID)
 
-  # Filter out excluded IDs using base::subset
-  id_list <- subset(id_list, !id_list %in% excluded_ids)
+  # Load the list of wrong IDs to exclude from scraping
+  wrongs1 <- readr::read_csv("~/Desktop/MASTER_all_wrongs.csv")
+  wrong_ids <- wrongs1$WrongIDs
 
-  fc <- cache_filesystem(file.path(".cache"))
-  GET_m <- memoise::memoise(httr::GET, cache = fc)
+  # Initialize data frames to store physicians' data and wrong IDs
+  Physicians <- data.frame()
+  WrongIDs <- c()
 
-  # Initialize the list to store physicians' data
-  Physicians <- list()  # Change this to a list
-
-  # Initialize the vector to store wrong IDs
-  WrongIDs <- NULL
+  # API Inputs
+  base <- "https://api.abog.org/"
+  endpoint <- "diplomate/"
+  action <- "/verify"
 
   for (id in id_list) {
-    r <- paste("https://api.abog.org/diplomate/", id, "/verify", sep = "")
-    ph_r <- GET_m(r)
-    ph_json <- httr::content(x = ph_r, as = "text", encoding = "UTF-8")
-    ph_data <- jsonlite::fromJSON(ph_json, flatten = TRUE)
+    # Check if the ID is in the list of wrong IDs
+    if (id %in% wrong_ids) {
+      cat("Skipping wrong ID:", id, "\n")
+      next
+    }
 
-    if (length(ph_data) != 0) {
-      # Check if 'userid' column is present in the data
-      if (!"userid" %in% colnames(ph_data)) {
-        # If 'userid' is missing, create a placeholder
-        ph_data$userid <- NA
+    # Create the API URL for the current ID
+    url <- paste(base, endpoint, id, action, sep = "")
+
+    # Send a GET request through Tor
+    ph_r <- httr::GET(url, use_proxy(paste0("socks5://localhost:", torPort)))
+
+    # Check if the request was successful
+    if (ph_r$status_code == 200) {
+      # Parse JSON response
+      ph_data <- jsonlite::fromJSON(httr::content(ph_r, as = "text"))
+
+      # Check if the response is not empty
+      if (length(ph_data) > 0) {
+        # Add ID and current timestamp to the data
+        ph_data$ID <- id
+        ph_data$DateTime <- Sys.time()
+
+        # Append the data to the Physicians data frame
+        Physicians <- dplyr::bind_rows(Physicians, ph_data)
+
+        cat("Scraped data for ID:", id, "\n")
+        cat("Total records scraped:", nrow(Physicians), "\n")
+      } else {
+        cat("Empty response for ID:", id, "\n")
+        WrongIDs <- c(WrongIDs, id)
       }
-
-      ph_data <- ph_data %>%
-        dplyr::mutate(ID = id) %>%
-        dplyr::mutate(DateTime = Sys.time())
-
-      Physicians <- list(Physicians, ph_data)  # Append the data to the list
-
-      # Debugging: Print ph_data
-      print(head(ph_data))
     } else {
+      cat("Request failed for ID:", id, "\n")
       WrongIDs <- c(WrongIDs, id)
     }
 
-    Sys.sleep(0.5)
+    # Sleep for a while to prevent server overload
+    Sys.sleep(1)
   }
 
-  # Bind the list elements together into a single dataframe
-  Physicians <- dplyr::bind_rows(Physicians)
+  # Define file names
+  physicians_file <- paste("Physicians (", startID, "-", endID, ") (", Sys.time(), ").csv", sep = "")
+  wrong_ids_file <- paste("Wrong IDs (", startID, "-", tail(WrongIDs, 1), ") (", Sys.time(), ").csv", sep = "")
 
-  # Define the file name for Physicians
-  file_name_physicians <- paste("Physicians (", startID, "-", endID, ") (", Sys.time(), ").csv", sep = "")
+  # Write data frames to CSV files
+  write.csv(Physicians, physicians_file, row.names = FALSE)
+  write.csv(data.frame(WrongIDs = WrongIDs), wrong_ids_file, row.names = FALSE)
 
-  # Define the full file path for Physicians
-  file_path_physicians <- "data/"  # Replace with the actual directory path where you want to save the Physicians file
-  full_file_path_physicians <- file.path(file_path_physicians, file_name_physicians)
+  cat("CSV file for Physicians saved as:", physicians_file, "\n")
+  cat("CSV file for Wrong IDs saved as:", wrong_ids_file, "\n")
 
-  # Save the CSV file for Physicians
-  write.csv(Physicians, full_file_path_physicians, row.names = FALSE)
-
-  # Define the file name for WrongIDs
-  file_name_wrongids <- paste("Wrong IDs (", startID, "-", tail(WrongIDs, 1), ") (", Sys.time(), ").csv", sep = "")
-
-  # Define the full file path for WrongIDs
-  file_path_wrongids <- "data/"  # Replace with the actual directory path where you want to save the WrongIDs file
-  full_file_path_wrongids <- file.path(file_path_wrongids, file_name_wrongids)
-
-  # Save the CSV file for WrongIDs
-  write.csv(data.frame(WrongIDs = WrongIDs), full_file_path_wrongids, row.names = FALSE)
-
-  # Display a message with the name and path of the saved files
-  cat("CSV file for Physicians saved as:", full_file_path_physicians, "\n")
-  cat("CSV file for Wrong IDs saved as:", full_file_path_wrongids, "\n")
-
-  # Return the Physicians data as a dataframe
   return(Physicians)
 }
+
+# Example usage with Tor port 9150 (update with your Tor port if different)
+# scrape_physicians_data_with_tor(startID = 9045923, endID = 9055923, torPort = 9150)
