d0919d0ed997731cb0eb664d7c11c09916cfd378
Mufflyt <tyler.muffly@dhha.org>
2023-11-24 13:36:49 -0700

    Creating a vignette for "validate_and_remove_invalid_npi.R" and add the toy data set of acog_presidents.rda.
    Former-commit-id: 70f6de42f23bdb2dba8547810e944b74e95d29d4

--- R/validate_and_remove_invalid_npi.R
+++ R/validate_and_remove_invalid_npi.R
@@ -7,38 +7,49 @@
 #'
 #' @return A dataframe containing valid NPI numbers.
 #'
+#' @import npi
+#' @import readr
+#' @import dplyr
+#'
 #' @examples
-#' input_csv_path <- "~/Dropbox (Personal)/workforce/subspecialists_only.csv"
+#' input_data <- "~/Dropbox (Personal)/workforce/subspecialists_only.csv"
 #' valid_df <- validate_and_remove_invalid_npi(input_csv_path)
 #'
 #' @export
-validate_and_remove_invalid_npi <- function(input_csv_path) {
+validate_and_remove_invalid_npi <- function(input_data) {
   library(dplyr)
-  library(readr)
   library(npi)  # Load the npi package
+  library(readr)
 
-  # Read the CSV file into a dataframe
-  df <- read_csv(input_csv_path)
+  if (is.data.frame(input_data)) {
+    # Input is a dataframe
+    df <- input_data
+  } else if (is.character(input_data)) {
+    # Input is a file path to a CSV
+    df <- readr::read_csv(input_data)
+  } else {
+    stop("Input must be a dataframe or a file path to a CSV.")
+  }
 
   # Remove rows with missing or empty NPIs
   df <- df %>%
-    filter(!is.na(npi) & npi != "")
+    #head(5) %>%. #for testing only
+    dplyr::filter(!is.na(npi) & npi != "")
 
   # Add a new column "npi_is_valid" to indicate NPI validity
   df <- df %>%
-    mutate(npi_is_valid = sapply(npi, function(x) {
+    dplyr::mutate(npi_is_valid = sapply(npi, function(x) {
       if (is.numeric(x) && nchar(x) == 10) {
         npi::npi_is_valid(as.character(x))
       } else {
         FALSE
       }
     })) %>%
-    filter(!is.na(npi_is_valid) & npi_is_valid)
+    dplyr::filter(!is.na(npi_is_valid) & npi_is_valid)
 
   # Return the valid dataframe with the "npi_is_valid" column
   return(df)
-  readr::write_csv(valid_df, "data/subspecialists_valid_npi.csv")
 }
 # Example usage:
-#input_csv_path <- "~/Dropbox (Personal)/workforce/subspecialists_only.csv"  # Replace with the path to your CSV file
+#input_data <- "~/Dropbox (Personal)/workforce/subspecialists_only.csv"  # Replace with the path to your CSV file
 #valid_df <- validate_and_remove_invalid_npi(input_csv_path)
