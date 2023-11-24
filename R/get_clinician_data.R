b769514cbf4bc7518382fc1c287d19f5ffa19fb1
Mufflyt <tyler.muffly@dhha.org>
2023-11-24 13:36:49 -0700

    Creating a vignette for "validate_and_remove_invalid_npi.R" and add the toy data set of acog_presidents.rda.
    Former-commit-id: 70f6de42f23bdb2dba8547810e944b74e95d29d4

--- R/get_clinician_data.R
+++ R/get_clinician_data.R
@@ -32,76 +32,65 @@
 #' #' @import zipcodeR
 #' #'
 #' #' @export
-#' fc <- cache_filesystem(file.path(".cache"))
-#' gc()
-#'
-#' ## Output
-#' df_updated <- NULL
-#'
-#' retrieve_clinician_data <- function(input_csv_path) {
-#'   #input_csv_path <- ("~/Dropbox (Personal)/workforce/subspecialists_only.csv") #for testing
-#'   library(provider)
-#'   library(dplyr)
-#'   library(purrr)
-#'   library(readr)
-#'   library(tidyr)
-#'   library(lubridate)
-#'   library(memoise)
-#'   library(zipcodeR)
-#'
-#'   # Load libraries
-#'   remotes::install_github("andrewallenbruce/provider")
-#'
-#'   # Create a memoized version of the clinicians function
-#'   memoized_clinicians <- memoise(provider::clinicians, cache = fc)
-#'
-#'   # Read the CSV file into a dataframe
-#'   df <- readr::read_csv(input_csv_path) %>%
-#'     dplyr::filter(!is.na(npi)) %>%
-#'     dplyr::distinct(npi, .keep_all = TRUE) %>%
-#'     dplyr::mutate(npi = as.numeric(npi))
-#'
-#'   #df <- df %>% head(10) # For testing
-#'
-#'   # Function to retrieve clinician data for a single NPI
-#'   get_clinician_data <- function(npi) {
-#'     if (!is.numeric(npi) || nchar(npi) != 10) {
-#'       cat("Invalid NPI:", npi, "\n")
-#'       return(NULL)  # Skip this NPI
-#'     }
-#'
-#'     # Use the memoized function to retrieve clinician data
-#'     clinician_info <- memoized_clinicians(npi = npi)
-#'
-#'     clinician_info <- provider::clinicians(npi = npi)
-#'     if (is.null(clinician_info)) {
-#'       cat("No results for NPI:", npi, "\n")
-#'     } else {
-#'       print(clinician_info)  # Print the clinician data as it comes out
-#'     }
-#'     #return(clinician_info)
-#'     Sys.sleep(1)
-#'   }
-#'
-#'   # Loop through the "npi" column and get clinician data
-#'   df_updated <- df %>%
-#'     dplyr::mutate(row_number = row_number()) %>%
-#'     dplyr::mutate(clinician_data = purrr::map(npi, get_clinician_data)) %>%
-#'     tidyr::unnest(clinician_data, names_sep = "_") %>%
-#'     dplyr::distinct(npi, .keep_all = TRUE)
-#'
-#'   # Save the updated dataframe as a CSV with the current date and time in the filename
-#'   timestamp <- format(Sys.time(), "%Y%m%d%H%M%S")
-#'   output_csv <- paste("data/updated_data_", timestamp, ".csv", sep = "")
-#'   readr::write_csv(df_updated, file = output_csv)
-#'
-#'   # Return the path to the saved CSV file
-#'   cat("Updated data saved as:", output_csv, "\n")
-#'
-#'   return(df_updated)
-#' }
-#' #Use Case
-#' #
-#' # Call the retrieve_clinician_data function with an NPI value
-#' # input_csv_path <- ("~/Dropbox (Personal)/workforce/subspecialists_only.csv")
-#' # clinician_data <- retrieve_clinician_data(input_csv_path)
+# fc <- cache_filesystem(file.path(".cache"))
+# gc()
+
+#
+# ## Output
+# df_updated <- NULL
+#
+# retrieve_clinician_data <- function(input_csv_path) {
+#   #input_csv_path <- ("~/Dropbox (Personal)/workforce/subspecialists_only.csv") #for testing
+#   library(provider)
+#   library(dplyr)
+#   library(purrr)
+#   library(readr)
+#   library(tidyr)
+#   library(lubridate)
+#   library(memoise)
+#   library(zipcodeR)
+#
+#   # Load libraries
+#   remotes::install_github("andrewallenbruce/provider")
+#
+#   # Read the CSV file into a dataframe
+#   df <- readr::read_csv(input_csv_path) %>%
+#     dplyr::filter(!is.na(npi)) %>%
+#     dplyr::distinct(npi, .keep_all = TRUE) %>%
+#     dplyr::mutate(npi = as.numeric(npi))
+#
+#   #df <- df %>% head(5) # For testing
+#
+#   # Function to retrieve clinician data for a single NPI
+#   # get_clinician_data <- function(npi) {
+#   #   if (!is.numeric(npi) || nchar(npi) != 10) {
+#   #     cat("Invalid NPI:", npi, "\n")
+#   #     return(NULL)  # Skip this NPI
+#   #   }
+#   df <- validate_and_remove_invalid_npi()
+#
+#
+#   clinician_info <- provider::clinicians(npi = npi)
+#     if (is.null(clinician_info)) {
+#       cat("No results for NPI:", npi, "\n")
+#     } else {
+#       print(clinician_info)  # Print the clinician data as it comes out
+#     }
+#     #return(clinician_info)
+#     Sys.sleep(1)
+#   }
+#
+#   # Loop through the "npi" column and get clinician data
+#   df_updated <- df %>%
+#     dplyr::mutate(row_number = row_number()) %>%
+#     dplyr::mutate(clinician_data = purrr::map(npi, get_clinician_data)) %>%
+#     tidyr::unnest(clinician_data, names_sep = "_") %>%
+#     dplyr::distinct(npi, .keep_all = TRUE)
+#
+#   return(df_updated)
+# }
+# #Use Case
+# #
+# # Call the retrieve_clinician_data function with an NPI value
+# input_csv_path <- ("~/Dropbox (Personal)/workforce/subspecialists_only.csv")
+# clinician_data <- retrieve_clinician_data(input_csv_path)
--- README.md
+++ README.md
@@ -113,7 +113,7 @@ obgyn_taxonomy <- tyler::taxonomy %>% filter(str_detect(`Classification`, fixed(
 ```
 
 ### Searching for Data: `tyler::search_by_taxonomy`
-This function searches the NPI Database for healthcare providers based on a taxonomy description.  This helps confirm outside data about subspecialist provider counts and fill in the gaps for providers who are not board-certified but are practicing (board-eligible).  This data can be matched to other databases.  Please see `Exploratory/workforce/subspecialists_only` for more code on how to do this.  The nice thing is that all these search results will come with an NPI.  
+This function searches the NPI Database for healthcare providers based on a taxonomy description.  The `search_by_taxonomy` function is a wrapper on the `npi::npi_search` accessing the registry's Version 2.1 API.  Many thanks to the author and maintainers of the `npi` package for their amazing work.This helps confirm outside data about subspecialist provider counts and fill in the gaps for providers who are not board-certified but are practicing (board-eligible).  This data can be matched to other databases.  Please see `Exploratory/workforce/subspecialists_only` for more code on how to do this.  The nice thing is that all these search results will come with an NPI.  
 ```r
 # Example usage with multiple taxonomy descriptions:
 data <- tyler::search_by_taxonomy(c("Gynecologic Oncology",
