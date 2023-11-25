7a344bf2aa97fc84ba80e40b0d563b123669dd59
Mufflyt <tyler.muffly@dhha.org>
2023-11-24 21:53:31 -0700

    fips data set.
    Former-commit-id: 5d132ab7b4ca98f7ae35b87f55eb85ab142ca958

---
+++ R/fips.R
@@ -0,0 +1,9 @@
+#' Data of FIPS codes
+#'
+#' 3 columns with 51 observations.
+#'
+#' @format A tibble with 243 rows and 10 variables:
+#' \describe{
+#' }
+#' @source \url{https://github.com/kjhealy/fips-codes/blob/master/state_and_county_fips_master.csv}
+"fips"
