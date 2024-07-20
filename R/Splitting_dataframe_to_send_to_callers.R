5b9ac0afd0433eadecf05295f0fe73215648d003
Mufflyt <tyler.muffly@dhha.org>
2024-07-20 12:48:39 -0600

    Added more testing and changes to functions.
    Former-commit-id: b2066d9fe061e1990dcf218cc05ed8bb16a19047

--- R/Splitting_dataframe_to_send_to_callers.R
+++ R/Splitting_dataframe_to_send_to_callers.R
@@ -23,10 +23,10 @@
 #' }
 
 split_and_save <- function(data_or_path, output_directory, lab_assistant_names,
-                                    seed = 1978,
-                                    complete_file_prefix = "complete_non_split_version_",
-                                    split_file_prefix = "",
-                                    recursive_create = TRUE) {
+                           seed = 1978,
+                           complete_file_prefix = "complete_non_split_version_",
+                           split_file_prefix = "",
+                           recursive_create = TRUE) {
 
   # Read the data from file if path provided
   if (is.data.frame(data_or_path)) {
