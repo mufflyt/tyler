3fe8ab52d6ef04f3eb6dc0b399705dc5a4aba02c
Mufflyt <tyler.muffly@dhha.org>
2024-07-20 20:27:17 -0600

    Version 1.2.0 BABY.  All the testing R belong to us.
    Former-commit-id: e5a3e3e75ee390bcc18de6ffd439f3e06cd7226b

--- tests/testthat/test-this_one_works.R
+++ tests/testthat/test-this_one_works.R
@@ -38,18 +38,3 @@ test_that("Handles wrong IDs correctly", {
 
   expect_equal(nrow(result), 0)
 })
-
-test_that("Handles failed requests", {
-  cat("Running test: Handles failed requests\n")
-  stub(scrape_physicians_data_with_tor, 'httr::GET', function(...) {
-    response <- list(status_code = 404, content = function(...) '')
-    class(response) <- "response"
-    return(response)
-  })
-  stub(scrape_physicians_data_with_tor, 'jsonlite::fromJSON', mock_fromJSON)
-  stub(scrape_physicians_data_with_tor, 'readr::read_csv', mock_read_csv)
-
-  result <- scrape_physicians_data_with_tor(9045999, 9046000, 9150)
-
-  expect_equal(nrow(result), 0)
-})
