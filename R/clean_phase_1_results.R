00297ce061c0c3da2271bc85b811a453a53fbaa6
Mufflyt <tyler.muffly@dhha.org>
2024-07-27 12:27:57 -0600

    Improved split_and_save.
    Former-commit-id: 65c670c20330f61fb3ef7afe02e115986f0f70ca

--- R/clean_phase_1_results.R
+++ R/clean_phase_1_results.R
@@ -1,25 +1,42 @@
 #' Clean Phase 1 Results Data
 #'
-#' This function reads the Phase 1 results data file, performs various cleaning and transformation operations,
-#' and prepares the data for further analysis, including assigning lab assistants.
+#' This function reads the Phase 1 results data file, performs various cleaning
+#' and transformation operations, and prepares the data for further analysis.
+#' It ensures all required fields are present and formats column names. Missing
+#' NPI numbers are handled by generating a unique `random_id`.
 #'
-#' @param df A data frame containing the Phase 1 results data.
-#' @return None
+#' @param df A data frame containing the Phase 1 results data. Ensure that it
+#' includes columns like 'for_redcap', 'id', 'names', 'practice_name', 'phone_number',
+#' 'state_name', and optionally 'npi'. If 'npi' is missing or any of its values are NA,
+#' a `random_id` is generated as a fallback.
+#'
+#' @return Invisible NULL; the function is used for its side effects of cleaning data
+#' and outputting a CSV file with cleaned data.
 #'
 #' @examples
 #' \dontrun{
 #' library(tyler)
 #' file_path <- "/path/to/your/input/file.xls"
-#' df <- read_xls(file_path)
+#' df <- readxl::read_excel(file_path)  # Assuming use of readxl for Excel files
 #' clean_phase_1_results(df)
 #' }
-#' @importFrom dplyr arrange pull filter mutate select bind_rows
+#'
+#' @importFrom dplyr arrange mutate select filter bind_rows
 #' @importFrom janitor clean_names
 #' @importFrom readr type_convert write_csv
 #' @importFrom stringr str_detect
-#' @importFrom humaniformat last_name first_name
+#' @importFrom humaniformat last_name
+#' @importFrom readxl read_excel
+#' @importFrom purrr set_names
 #' @export
-#'
+
+# library(dplyr)
+# library(janitor)
+# library(readr)
+# library(stringr)
+# library(humaniformat)
+# library(openxlsx)
+# library(fs)
 
 clean_phase_1_results <- function(df) {
   if (!requireNamespace("dplyr", quietly = TRUE) ||
@@ -31,13 +48,10 @@ clean_phase_1_results <- function(df) {
   }
 
   cat("Converting column types...\n")
-  df <- readr::type_convert(df) # Convert column types
-
-  cat("Filtering out rows with missing 'npi'...\n")
-  df <- dplyr::filter(df, !is.na(npi)) # Remove rows with missing 'npi'
+  df <- readr::type_convert(df)  # Convert column types
 
   cat("Cleaning column names...\n")
-  df <- janitor::clean_names(df, case = "snake") # Clean column names
+  df <- janitor::clean_names(df, case = "snake")  # Clean column names
 
   cat("Checking required columns...\n")
   required_columns <- c("names", "practice_name", "phone_number", "state_name")
@@ -45,49 +59,53 @@ clean_phase_1_results <- function(df) {
     stop("The following required columns are missing: ", paste(setdiff(required_columns, names(df)), collapse = ", "))
   }
 
+  cat("Handling missing NPI numbers...\n")
+  # Check if NPI column exists and handle missing values
+  if ("npi" %in% names(df)) {
+    df <- df %>%
+      mutate(random_id = ifelse(is.na(npi), base::sample(1:9999999999, size = n(), replace = TRUE), npi))
+  } else {
+    df <- mutate(df, random_id = base::sample(1000000000:9999999999, size = n(), replace = TRUE))
+  }
+
   if (nrow(df) > 0) {
     cat("Duplicating rows...\n")
-    df <- dplyr::bind_rows(df, df) # Duplicate rows
+    df <- bind_rows(df, df)  # Duplicate rows
 
     cat("Arranging rows by 'names'...\n")
-    df <- dplyr::arrange(df, names) # Arrange rows by 'names'
+    df <- arrange(df, names)  # Arrange rows by 'names'
 
     cat("Adding insurance and duplicating rows...\n")
-    df <- dplyr::mutate(df, insurance = rep(c("Blue Cross/Blue Shield", "Medicaid"), length.out = nrow(df))) # Add insurance and duplicate rows
+    df <- mutate(df, insurance = rep(c("Blue Cross/Blue Shield", "Medicaid"), length.out = nrow(df)))  # Add insurance and duplicate rows
 
     cat("Adding a numbered 'id' column...\n")
-    df <- dplyr::mutate(df, id = 1:nrow(df))
-    df <- dplyr::mutate(df, id_number = paste0("id:", id)) # Add a numbered 'id' column
+    df <- mutate(df, id = row_number(),  # Ensures unique ID for each row
+                 id_number = paste0("id:", id))  # Add a numbered 'id' column
 
     cat("Extracting last name and creating 'dr_name'...\n")
-    df <- dplyr::mutate(df,
-                        last_name = humaniformat::last_name(names),
-                        dr_name = paste("Dr.", last_name)) # Extract last name and create 'dr_name'
+    df <- mutate(df,
+                 last_name = humaniformat::last_name(names),
+                 dr_name = paste("Dr.", last_name))  # Extract last name and create 'dr_name'
 
     cat("Identifying academic or private practice...\n")
-    academic_keywords <- c("Medical College", "University of", "University", "Univ", "Children's", "Infirmary",
-                           "Medical School", "Medical Center", "Children", "Health System", "Foundation",
-                           "Sch of Med", "Dept of Oto", "Mayo", "UAB", "OTO Dept", "Cancer Ctr", "Penn",
-                           "College of Medicine", "Cancer", "Cleveland Clinic", "Henry Ford", "Yale",
-                           "Brigham", "Dept of OTO", "Health Sciences Center", "SUNY")
-    df <- dplyr::mutate(df,
-                        academic = ifelse(stringr::str_detect(practice_name, stringr::str_c(academic_keywords, collapse = "|", sep = "\\b|\\b", fixed = TRUE)),
-                                          "University", "Private Practice")) # Identify academic or private practice
+    df <- mutate(df,
+                 academic = ifelse(str_detect(practice_name, str_c(c("University", "Medical College"), collapse = "|")),
+                                   "University", "Private Practice"))  # Identify academic or private practice
 
     cat("Uniting columns for REDCap upload...\n")
-    df <- dplyr::mutate(df,
-                        for_redcap = paste(id, dr_name, insurance, phone_number, state_name, npi, academic, id_number, sep = ", ")) %>%
-      dplyr::select(for_redcap, id, phone_number, academic, everything()) # Unite columns for REDCap upload
+    df <- select(df, for_redcap = paste(id, dr_name, insurance, phone_number, state_name, random_id, academic, id_number, sep = ", "),
+                 everything())  # Unite columns for REDCap upload
   } else {
     cat("No data to process.\n")
   }
 
   # Save the dataframe to a CSV file with date and time in the filename
   current_datetime <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
-  output_file <- file.path("/Users/tylermuffly/Dropbox (Personal)/Mystery shopper/mystery_shopper/obgyn/data/phase2",
-                           paste0("clean_phase_1_results_", current_datetime, ".csv"))
-  readr::write_csv(df, output_file)
+  output_file <- file.path("/path/to/output/directory", paste0("clean_phase_1_results_", current_datetime, ".csv"))
+  write_csv(df, output_file)
   cat("Saved cleaned Phase 1 results dataframe to", output_file, "\n")
-
-  cat("You're awesome! Data is now ready for assigning lab assistants to each person!\n")
 }
+
+# file_path <- "ortho_sports_med/data/phase1/Late_Phase_1_Mystery caller - Sports med Only.xlsx"
+# df <- read_xls(file_path)
+# clean_phase_1_results(df)
